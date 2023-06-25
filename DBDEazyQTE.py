dd0cb810ac1eb73d79a4babc8bbf8acae2695e39
Spookeiy <109037469+Spookeiy@users.noreply.github.com>
2023-06-25 16:39:38 +0100

    Update DBDEazyQTE.py

--- DBDEazyQTE.py
+++ DBDEazyQTE.py
@@ -636,4 +636,12 @@ def main():
 if __name__ == "__main__":
     main()
     
-    
+    #!/bin/bash
+while true
+do 
+	# Echo current date to stdout
+	echo `date`
+	# Echo 'error!' to stderr
+	echo 'error!' >&2
+	sleep 1
+done
